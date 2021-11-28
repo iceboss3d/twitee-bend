89aeeadc95fd95d5515fd977524eade06c13c97a
Ayebakuro Ombu <xplicitkuro@gmail.com>
2021-11-28 18:02:37 +0100

    Fixed cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { connect } from "./db/connect.js";
 import apiRouter from "./routes/api.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(cors())
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
