ab936f73738240703528d47f33de80fe6b621503
Ayebakuro <ayebakuro.ombu@decagon.dev>
2021-11-28 16:07:27 +0100

    Updated port for heroku

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { connect } from "./db/connect.js";
 import apiRouter from "./routes/api.js";
 
 const app = express();
-const port = 8080;
+const port = precess.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
