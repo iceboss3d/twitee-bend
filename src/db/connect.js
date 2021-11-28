421b8f3403b0f883b299baf47289b60e0c46c061
Ayebakuro <ayebakuro.ombu@decagon.dev>
2021-11-28 16:04:50 +0100

    Fixed issues with ssl

--- src/db/connect.js
+++ src/db/connect.js
@@ -3,7 +3,16 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 const connect = async () => {
   try {
